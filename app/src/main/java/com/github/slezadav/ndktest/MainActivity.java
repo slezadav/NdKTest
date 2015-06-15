a2824e7178e3bae4aed070cb9eb33b0e657f16d5
david.slezak <david.slezak@firma.seznam.cz>
2015-06-15 09:21:32 +0200

    changed package name

--- app/src/main/java/com/github/slezadav/ndktest/MainActivity.java
+++ app/src/main/java/com/github/slezadav/ndktest/MainActivity.java
@@ -1,11 +1,10 @@
-package test.seznam.cz.ndktest;
+package com.github.slezadav.ndktest;
 
 //import android.support.v7.app.ActionBarActivity;
 // This line is not needed as we are not targetting older devices
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -23,23 +22,9 @@ public class MainActivity extends Activity {
         TextView tv = (TextView) findViewById(R.id.text);
         tv.setText(stringFromJNI("Ahoj"));
 
-        for(int i=10000;i<1000000000;i*=10){
-            long start=System.currentTimeMillis();
-            execTest(i);
-            Log.i("TAG","Java time for "+i+"  :  "+(System.currentTimeMillis()-start));
-            start=System.currentTimeMillis();
-            test(i);
-            Log.i("TAG","Cpp time for "+i+"  :  "+(System.currentTimeMillis()-start));
-        }
-
     }
 
-    private void execTest(int limit){
-        int j=0;
-        for(int i=0;i<limit;++i){
-            j=i/100;
-        }
-    }
+
 
 
 
--- app/src/main/jni/main.cpp
+++ app/src/main/jni/main.cpp
@@ -4,19 +4,20 @@
 #include <android/log.h>
 #include "logHelper.h"
 using namespace std;
+#define MAINACTIVITY(n) JNICALL Java_com_github_slezadav_ndktest_MainActivity_##n
 extern "C" {
- JNIEXPORT jstring JNICALL Java_test_seznam_cz_ndktest_MainActivity_stringFromJNI( JNIEnv* env,jobject thiz,jstring javaString);
- JNIEXPORT void JNICALL Java_test_seznam_cz_ndktest_MainActivity_test( JNIEnv* env,jobject thiz,jint limit);
+ JNIEXPORT jstring MAINACTIVITY(stringFromJNI)( JNIEnv* env,jobject thiz,jstring javaString);
+ JNIEXPORT void MAINACTIVITY(test)( JNIEnv* env,jobject thiz,jint limit);
 }
 
-JNIEXPORT jstring JNICALL Java_test_seznam_cz_ndktest_MainActivity_stringFromJNI( JNIEnv* env, jobject thiz,jstring javaString)
+JNIEXPORT jstring MAINACTIVITY(stringFromJNI)( JNIEnv* env, jobject thiz,jstring javaString)
       {
       string param=toCpp(env,javaString);
       LOGI("Param is %s\n", toPtr(param));
       string result="Hello from C++ over JNI! "+param;
       return toJava(env,result);
       }
-JNIEXPORT void JNICALL Java_test_seznam_cz_ndktest_MainActivity_test( JNIEnv* env, jobject thiz,jint limit)
+JNIEXPORT void MAINACTIVITY(test)( JNIEnv* env, jobject thiz,jint limit)
       {
       int j=0;
       for(int i=0;i<limit;++i){
